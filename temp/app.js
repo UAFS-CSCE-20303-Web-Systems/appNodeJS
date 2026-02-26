42699fb34e43f396203083b62e1cf0f1f04c7a1a
John Hightower <john.hightower@uafs.edu>
2026-02-26 10:32:36 -0600

    Updated

--- temp/app.js
+++ temp/app.js
@@ -1,24 +1,33 @@
 const http = require("node:http");
 const url = require("node:url");
+const queryString = require("node:querystring");
 
 const server = http.createServer(function (req, res) {
+
+
   //Parse URL on GET
-  parsedURL = url.parse(req.url, true);
-  let name = parsedURL.query.name;
+  if(req.method === 'GET'){
+    let parsedURL = url.parse(req.url, true);
+    let name = parsedURL.query.name;
+    console.log(parsedURL.query);
+  }
+ 
+  //Parse Request Body on POST
+  if (req.method === "POST") {
+    //Parse POST Request Body
+    let body = "";
+    let parsedData = "";
+    req.on("data", function (chunk) {
+      body = body + chunk.toString();
+    });
 
-  let body = "";
-  req.on("data", function (chunk) {
-    body = body + chunk.toString();
-  });
+    req.on("end", function () {
+      parsedData = queryString.parse(body);
+      console.log(parsedData);
+    });
+  }
 
-  req.on("end", function (chunk) {
-    body = body + chunk.toString();
-  });
-
-  console.log(req.url);
-  console.log(parsedURL.query.name);
-  console.log(parsedURL);
-  res.end(`<b>Hello ${name}!</b>`);
+  res.end(`<b>Hello World!</b>`);
 });
 
 server.listen(3000, function () {
